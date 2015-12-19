bc8f4e172663496207737c7432dbb14df24e81a4
zhidev <Doug@douglass-macbook-air.local>
2015-12-18 17:29:24 -0800

    Stuff Before I add in Alamofire and swiftyJSON: using this tutorial as a basis http://ashishkakkad.com/2015/10/how-to-use-alamofire-and-swiftyjson-with-swift-swift-2-ios-9-xcode-7/. Everything should be working fine except for getting the tweet table (Longitude,Latitude properly accessed, Twitter Login, Showing tweet table[push]). Still need: Properly getting correct tweet tables, settings tab

--- TweetsNearby/TweetTableViewController.swift
+++ TweetsNearby/TweetTableViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import TwitterKit
+import SwiftyJSON
+import Alamofire
 
 class TweetTableViewViewController: UITableViewController, TWTRTweetViewDelegate {
     let tweetTableReuseIdentifier = "TweetCell"
